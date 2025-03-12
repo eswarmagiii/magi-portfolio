24c31672f2787e069198e1e7f483d2731fd47093
eswarmagiii <eswarmagi@outlook.com>
2025-03-12 08:41:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSection(sectionId) {
+    gsap.to('.section', { opacity: 0, scale: 0.9, duration: 0.8, onComplete: function() {
+        document.querySelectorAll('.section').forEach(section => section.classList.remove('active'));
+        document.getElementById(sectionId).classList.add('active');
+        gsap.to('#' + sectionId, { opacity: 1, scale: 1, duration: 0.2 });
+    }});
+}
